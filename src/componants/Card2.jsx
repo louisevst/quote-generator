26b4a397f7b315490eed835817379b997f08e382
louisevst <louisevassart@gmail.com>
2023-03-13 11:40:42 +0100

    forgot to save card2 on last commit

--- src/componants/Card2.jsx
+++ src/componants/Card2.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Card2() {
   return (
-    <div className="w-[90%] rounded-full lg:h-[550px]  lg:w-[450px] lg:translate-x-[-15px] lg:translate-y-0 translate-y-[-15px] card2 bg-cover bg-center flex justify-center items-center p-14 flex-1">
+    <div className="w-[90%] rounded-full lg:h-[550px]  lg:w-[300px] lg:translate-x-[-15px] lg:translate-y-0 translate-y-[-15px] card2 bg-cover bg-center flex justify-center items-center p-14 flex-1">
       <p className="p-4 bg-blackGreen text-whitish text-white text-center text-2xl">
         Click on the button to generate a new quote
       </p>
